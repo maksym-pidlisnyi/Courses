4f123687c7c72466e6704a1e3094dcdbeb07eaa5
Dmitrii-Kh <actibagamma@gmail.com>
2021-04-26 20:00:18 +0300

    pricing plans feature list added + testimonials carousel

--- public/src/components/CourseDetailed.js
+++ public/src/components/CourseDetailed.js
@@ -62,23 +62,111 @@ class CourseDetailed extends React.Component {
                                         <h2>standard</h2>
                                         <div className="pricing-price">
                                             <span className="price">10</span>
-                                            <span>$</span>
+                                            <span className="currency">$</span>
                                         </div>
+                                        <a href="#" className="start-btn">start standard plan</a>
                                     </div>
-                                    <div className="pricing-plan-features"></div>
+                                    <div className="pricing-plan-features">
+                                        <ul className="features-list">
+                                            <li className="active-feature">Feature 1</li>
+                                            <li className="active-feature">Feature 2</li>
+                                            <li className="active-feature">Feature 3</li>
+                                            <li className="active-feature">Feature 4</li>
+                                            <li className="inactive-feature">Feature 5</li>
+                                            <li className="inactive-feature">Feature 6</li>
+                                            <li className="inactive-feature">Feature 7</li>
+                                        </ul>
+                                    </div>
                                 </div>
                                 <div className="pricing-plan">
                                     <div id="best-option">best option</div>
                                     <div className="pricing-plan-display">
                                             <h2>premium</h2>
                                             <div className="pricing-price">
-                                                <span className="price">10</span>
-                                                <span>$</span>
+                                                <span className="price">16</span>
+                                                <span className="currency">$</span>
                                             </div>
+                                        <a href="#" className="start-btn">start premium plan</a>
                                     </div>
-                                    <div className="pricing-plan-features"></div>
+                                    <div className="pricing-plan-features">
+                                        <ul className="features-list">
+                                            <li className="active-feature">Feature 1</li>
+                                            <li className="active-feature">Feature 2</li>
+                                            <li className="active-feature">Feature 3</li>
+                                            <li className="active-feature">Feature 4</li>
+                                            <li className="active-feature">Feature 5</li>
+                                            <li className="active-feature">Feature 6</li>
+                                            <li className="active-feature">Feature 7</li>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
+                        </div>
+                    </div>
+
+                    <div className="detailed-course-testimonials">
+                        <h1>what our customers saying</h1>
+                        <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+                            <div className="carousel-inner">
+                                <div className="carousel-item active">
+                                    <div className="testimonial-triple">
+                                        <div className="testimonial-card">
+                                            <div className="testi-header"> <span>img</span> <span>Name</span> </div>
+                                            <div className="testi-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid at commodi consectetur cum dolore ipsam magnam nam natus nisi repellat, sint sit tempore voluptas! Amet consequuntur fugit mollitia quaerat similique?</div>
+                                        </div>
+                                        <div className="testimonial-card">
+                                            <div className="testi-header"> <span>img</span> <span>Name</span> </div>
+                                            <div className="testi-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid at commodi consectetur cum dolore ipsam magnam nam natus nisi repellat, sint sit tempore voluptas! Amet consequuntur fugit mollitia quaerat similique?</div>
+                                        </div>
+                                        <div className="testimonial-card">
+                                            <div className="testi-header"> <span>img</span> <span>Name</span> </div>
+                                            <div className="testi-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid at commodi consectetur cum dolore ipsam magnam nam natus nisi repellat, sint sit tempore voluptas! Amet consequuntur fugit mollitia quaerat similique?</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="carousel-item">
+                                    <div className="testimonial-triple">
+                                        <div className="testimonial-card">
+                                            <div className="testi-header"> <span>img</span> <span>Name</span> </div>
+                                            <div className="testi-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid at commodi consectetur cum dolore ipsam magnam nam natus nisi repellat, sint sit tempore voluptas! Amet consequuntur fugit mollitia quaerat similique?</div>
+                                        </div>
+                                        <div className="testimonial-card">
+                                            <div className="testi-header"> <span>img</span> <span>Name</span> </div>
+                                            <div className="testi-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid at commodi consectetur cum dolore ipsam magnam nam natus nisi repellat, sint sit tempore voluptas! Amet consequuntur fugit mollitia quaerat similique?</div>
+                                        </div>
+                                        <div className="testimonial-card">
+                                            <div className="testi-header"> <span>img</span> <span>Name</span> </div>
+                                            <div className="testi-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid at commodi consectetur cum dolore ipsam magnam nam natus nisi repellat, sint sit tempore voluptas! Amet consequuntur fugit mollitia quaerat similique?</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="carousel-item">
+                                    <div className="testimonial-triple">
+                                        <div className="testimonial-card">
+                                            <div className="testi-header"> <span>img</span> <span>Name</span> </div>
+                                            <div className="testi-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid at commodi consectetur cum dolore ipsam magnam nam natus nisi repellat, sint sit tempore voluptas! Amet consequuntur fugit mollitia quaerat similique?</div>
+                                        </div>
+                                        <div className="testimonial-card">
+                                            <div className="testi-header"> <span>img</span> <span>Name</span> </div>
+                                            <div className="testi-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid at commodi consectetur cum dolore ipsam magnam nam natus nisi repellat, sint sit tempore voluptas! Amet consequuntur fugit mollitia quaerat similique?</div>
+                                        </div>
+                                        <div className="testimonial-card">
+                                            <div className="testi-header"> <span>img</span> <span>Name</span> </div>
+                                            <div className="testi-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid at commodi consectetur cum dolore ipsam magnam nam natus nisi repellat, sint sit tempore voluptas! Amet consequuntur fugit mollitia quaerat similique?</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a className="carousel-control-prev" href="#carouselExampleControls" role="button"
+                               data-slide="prev">
+                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span className="sr-only">Previous</span>
+                            </a>
+                            <a className="carousel-control-next" href="#carouselExampleControls" role="button"
+                               data-slide="next">
+                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span className="sr-only">Next</span>
+                            </a>
                         </div>
                     </div>
 
